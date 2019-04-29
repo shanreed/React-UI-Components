286a09454840df386eb2231df89fad422062605c
shanreed <srtech5set@gmail.com>
2019-04-29 16:31:05 -0700

    added header and card components contents

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+
+function CardContents() {
+    return (
+        <div className = 'card-contents'>
+            <h2>Get started with React</h2>
+            <p>React makes it painless to create interactive UIs. Design simple views for
+                each state in your application
+            </p>
+        </div>
+
+    );
+}
+
+export default CardContents;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className = 'header-title-container'>
+            <h1>Lambda School @LambdaSchool</h1>
+        </div>
+    );
+}
+
+export default HeaderTitle;
