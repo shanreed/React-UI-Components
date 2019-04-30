20a6ce71b3704769ae684d63127003961a674e2b
shanreed <srtech5set@gmail.com>
2019-04-29 23:24:25 -0700

    attemped the stretch

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js';
 import CardContainer from './components/CardComponents/CardContainer.js';       
-
+import Footer from './components/FooterComponents/Footer.js';
 const App = () => {
   return (
     <div className = 'container'>
     <div>
       <HeaderContainer/>
       <CardContainer/>
+      <Footer/>
     </div>
     </div>
   );
--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,5 +3,14 @@ import './Footer.css';
 
 
 const Footer = () => {
-    
+    return (
+    <div className = 'footer'>  
+        <div className = 'talk-bubble'><i class='far fa-comment'></i></div>
+        <div className = 'circle'><i class="fa fa-envelope-o"></i></div>
+        <div className = 'heart'><i class="fa fa-heart-o">4</i></div>
+        <div className = 'envelop'><i class="fa fa-envelope-o"></i></div>
+    </div>
+    );
 }
+
+export default Footer;
